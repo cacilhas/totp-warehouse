aebb4f54badc5b0af210805f21bcffa4a5c3ec25
Rodrigo Cacilhas <batalema@cacilhas.info>
2020-02-24 12:52:04 -0500

    run only one instance at time

--- main.go
+++ main.go
@@ -3,22 +3,38 @@ package main
 import (
 	"os"
 	"os/exec"
+	"path"
 	"time"
 
+	"github.com/cacilhas/totp-warehouse/config"
 	"github.com/cacilhas/totp-warehouse/gui"
 )
 
 func main() {
 	if search(os.Args, "-nofork") {
-		gui.Start()
-
+		start()
 	} else {
-		// Fork
-		args := append(os.Args[1:], "-nofork")
-		exec.Command(os.Args[0], args...).Start()
-		time.Sleep(2 * time.Second)
-		os.Exit(0)
+		fork()
 	}
+}
+
+func start() {
+	configdir := config.ConfigDir()
+	lockname := path.Join(configdir, "lock")
+	if lockfile, err := os.OpenFile(lockname, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600); err == nil {
+		lockfile.Close()
+	} else {
+		return // TOTP Warehouse is running yet
+	}
+	defer os.Remove(lockname)
+	gui.Start()
+}
+
+func fork() {
+	args := append(os.Args[1:], "-nofork")
+	exec.Command(os.Args[0], args...).Start()
+	time.Sleep(2 * time.Second)
+	os.Exit(0) // force quit
 }
 
 func search(args []string, element string) bool {
